17ce672180ffdaa705e5e5058466bf81eb965dbd
Ikbal <ikbalnayem000@gmail.com>
2024-12-09 14:57:21 +0600

    footer height reduce

--- components/home/hero-section.tsx
+++ components/home/hero-section.tsx
@@ -108,7 +108,7 @@ const HeroSection: FC = () => {
 					</SwiperSlide>
 				))}
 			</Swiper>
-			<div className='absolute bottom-0 left-0 right-0 h-20 bg-gradient-to-t from-background to-transparent z-10' />
+			<div className='absolute bottom-0 left-0 right-0 h-14 bg-gradient-to-t from-background to-transparent z-10' />
 		</div>
 	);
 };
